2b1269972be31f2ad90887dd73b2aacbb868fb8f
RickyRen12 <146994057+RickyRen12@users.noreply.github.com>
2024-10-10 09:29:38 -0400

    i tried my best :(

--- Scripts/REALenemy.gd
+++ Scripts/REALenemy.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 var speed =90
 var player_chase = false
 var player = null
+var health_amount : int = 3
 
 func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position)/speed
+	move_and_slide()
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	player = body
@@ -16,3 +19,11 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+
+func _on_hurt_box_area_entered(area: Area2D):
+	print("HURTTTTTTTTTT", health_amount)
+	if area.get_parent().has_method("get_damage_amount"):
+		var node = area.get_parent() as Node
+		health_amount -= node.get_damage_amount
+		print("AHHH", health_amount)
--- Scripts/knife.gd
+++ Scripts/knife.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var speed = 300
+var damage_amount : int = 1
 
 func _ready():
 	set_as_top_level(true)
@@ -12,3 +13,13 @@ func _process(delta):
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
+
+func get_damage_amount() -> int:
+	return damage_amount
+
+func _on_area_entered(area: Area2D) -> void:
+	print("area")
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("body")
