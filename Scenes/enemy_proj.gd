eee05fd0771b65c68d687ba0ec165d3ad82c859d
RickyRen12 <146994057+RickyRen12@users.noreply.github.com>
2024-11-26 09:37:55 -0500

    enemy

---
+++ Scenes/enemy_proj.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var speed = 300
+var damage_amount_player : int = 1
+
+func _ready():
+	set_as_top_level(true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	queue_free()
+
+func get_damage_amount_player() -> int:
+	return damage_amount_player	
+
+func _on_area_entered(area: Area2D) -> void:
+	print("area")
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("walls")):
+		queue_free()
+	print("body")
+
+func holder():
+	print("please")
--- Scripts/REALenemy.gd
+++ Scripts/REALenemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var EnemyDeath = preload("res://Scenes/EnemyDeath.tscn")
-var proj = preload("res://Scenes/knife.tscn") 
+var proj = preload("res://Scenes/enemyProj.tscn") 
 var speed = 200
 var player_chase = false
 var will_shoot = false
@@ -47,16 +47,16 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		print("RARRRRRRRRRRRRRRRRRRRRRRRRAHHHHHHHHHHHHHH")
 		will_shoot = false
 
-#
-#func _on_hurt_box_area_entered(area: Area2D):
-	#print("HURTTTTTTTTTT", health_amount)
-	#if area.has_method("get_damage_amount"):
-		#var node = area as Node
-		#health_amount = health_amount - node.get_damage_amount()
-		#print("AHHH", health_amount)
-		#
-		#if health_amount <= 0:
-			#var EnemyDeath_instance = EnemyDeath.instantiate() as Node2D
-			#EnemyDeath_instance.global_position = global_position
-			#get_parent().add_child(EnemyDeath_instance)
-			#queue_free()
+
+func _on_hurt_box_area_entered(area: Area2D):
+	print("HURTTTTTTTTTT", health_amount)
+	if area.has_method("get_damage_amount"):
+		var node = area as Node
+		health_amount = health_amount - node.get_damage_amount()
+		print("AHHH", health_amount)
+		
+		if health_amount <= 0:
+			var EnemyDeath_instance = EnemyDeath.instantiate() as Node2D
+			EnemyDeath_instance.global_position = global_position
+			get_parent().add_child(EnemyDeath_instance)
+			queue_free()
