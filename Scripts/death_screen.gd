0098dc96a1a4a0781b1785c94ee9e1d1c0d9a63e
ITSAKILLERQUEEN <checkersyt2464@gmail.com>
2025-02-07 09:51:52 -0500

    death screen

---
+++ Scripts/death_screen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_ReloadButton_pressed():
+	var reload = get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	pass
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+	pass
